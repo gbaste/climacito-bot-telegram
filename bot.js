583c931ba14d10399ee603e3476632f1fdb0dfb0
gbaste <gbfabre@gmail.com>
2018-12-30 19:08:46 +0100

    first commit

---
+++ bot.js
@@ -0,0 +1,190 @@
+require("dotenv").config();
+
+const TelegramBot = require("node-telegram-bot-api");
+const weather = require("weather-js");
+
+const {
+  env: {
+    token
+  }
+} = process;
+
+const bot = new TelegramBot(token, {
+  polling: true
+});
+
+capitalizeFirstLetter = string => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+controlRainfall = string => {
+  if (!string) {
+    return 0;
+  }
+  return string;
+};
+
+emojisForDays = string => {
+  if (string === "Muy soleado" || string === "Soleado") {
+    return "☀️";
+  } else if (
+    string === "Prácticamente despejado" ||
+    string === "Parc. soleado"
+  ) {
+    return "🌤";
+  } else if (
+    string === "Parc. nublado" ||
+    string === "Muy nublado" ||
+    string === "Nublado"
+  ) {
+    return "⛅️";
+  } else if (
+    string === "Lluvia" ||
+    string === "Llovizna") {
+    return "🌧"
+  } else if (
+    string === "Tormentas" ||
+    string === "Chubascos"
+  ) {
+    return "⛈";
+  } else if (string === "Nieve") {
+    return "❄️"
+  }
+};
+
+bot.onText(/^\/start/, function (msg) {
+  var chatId = msg.chat.id;
+  var username = msg.from.username;
+
+  bot.sendMessage(
+    chatId,
+    "Hola, " +
+    username +
+    " soy un bot y mi nombre es Climacito" +
+    "\n" +
+    "Escribe /clima nombredelacity para informarte del tiempo"
+  );
+});
+
+bot.on("message", function (msg) {
+  const sayHello = "hola bot";
+
+  if (msg.from.first_name === undefined) {
+    const firstnameuser = "Usuario sin username";
+  }
+  const firstnameuser = msg.from.first_name;
+  if (
+    msg.text
+    .toString()
+    .toLowerCase()
+    .includes(sayHello)
+  ) {
+    bot.sendMessage(msg.chat.id, "Hola " + firstnameuser + ", como estás?");
+  }
+});
+
+bot.onText(/^\/clima (.+)/, function (msg, match) {
+  const chatId = msg.chat.id;
+  const city = match[1];
+
+  const opciones = {
+    search: city, // lugar es la ciudad que el usuario introduce
+    degreeType: "C", // Celsius
+    lang: "es-ES" // Lenguaje en el que devolverá los datos
+  };
+
+  weather.find(opciones, async function (err, result) {
+    if (err) {
+      // Si ocurre algun error...
+      bot.sendMessage(chatId, "El servidor no esta opeartivo");
+    } else if (!result[0]) {
+      bot.sendMessage(chatId, "La ciudad introducida no existe");
+    } else {
+      await bot.sendMessage(
+        chatId,
+        "Lugar: " +
+        result[0].location.name +
+        " " +
+        this.emojisForDays(result[0].current.skytext) +
+        "\n\nTemperatura: " +
+        result[0].current.temperature +
+        "ºC\n" +
+        "Estado del cielo: " +
+        result[0].current.skytext +
+        "\n" +
+        "Humedad: " +
+        result[0].current.humidity +
+        "%\n" +
+        "Dirección del viento: " +
+        result[0].current.winddisplay,
+
+        {
+          parse_mode: "Markdown"
+        }
+      );
+
+      const days = result[0].forecast.slice(2, result[0].forecast.length);
+
+      call = async days => {
+        const element = days[0];
+        await bot.sendMessage(
+          chatId,
+          this.capitalizeFirstLetter(element.day) +
+          " " +
+          this.emojisForDays(element.skytextday) +
+          "\n\n" +
+          "Temperatura minima: " +
+          element.low +
+          "\n" +
+          "Temperatura maxima: " +
+          element.high +
+          "\n" +
+          "Precipitaciones: " +
+          this.controlRainfall(element.precip) +
+          "%\n" +
+          "Estado del cielo: " +
+          element.skytextday, {
+            parse_mode: "Markdown"
+          }
+        );
+
+        if (days.length) {
+          days.splice(0, 1);
+
+          call(days);
+        }
+      };
+
+      call(days);
+
+
+      // VERSION ANTERIOR
+
+      // result[0].forecast.map((element, index) => {
+      //   if (index >= 2) {
+      //     return bot.sendMessage(
+      //       chatId,
+      //       this.capitalizeFirstLetter(element.day) +
+      //         " " +
+      //         this.emojisForDays(element.skytextday) +
+      //         "\n\n" +
+      //         "Temperatura minima: " +
+      //         element.low +
+      //         "\n" +
+      //         "Temperatura maxima: " +
+      //         element.high +
+      //         "\n" +
+      //         "Precipitaciones: " +
+      //         this.controlRainfall(element.precip) +
+      //         "%\n" +
+      //         "Estado del cielo: " +
+      //         element.skytextday,
+      //       {
+      //         parse_mode: "Markdown"
+      //       }
+      //     );
+      //   }
+      // });
+    }
+  });
+});
